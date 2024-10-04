fb1cec96b982f8c5de8ddc0e223e2e118bd8ada7
VitalyKon1 <vito.konsad@gmail.com>
2024-10-04 12:59:21 +0300

    init project, implemeted seconds_difference with asserts

---
+++ cs211-extra-task-1/main.cpp
@@ -0,0 +1,13 @@
+#include "extra-task-1.h"
+#include <iostream>
+#include <cassert>
+#include <cmath>
+#include <cfloat>
+
+int main() {
+	//seconds_difference assert
+	assert(fabs(seconds_difference(1800.0, 3600.0) - 1800.0) < DBL_EPSILON);
+	assert(fabs(seconds_difference(3600.0, 1800.0) - -1800.0) < DBL_EPSILON);
+	assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON);
+	assert(fabs(seconds_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
+}
