57b72a4f2c184f7b38e3efe8f2415b4fae8eae60
VitalyKon1 <vito.konsad@gmail.com>
2024-10-04 13:06:26 +0300

    implemented hours_difference with assert tests

--- cs211-extra-task-1/extra-task-1.cpp
+++ cs211-extra-task-1/extra-task-1.cpp
@@ -7,6 +7,7 @@ double seconds_difference(double time_1, double time_2)
 
 double hours_difference(double time_1, double time_2)
 {
+    return seconds_difference(time_1, time_2) / 3600;
     /*
         Return the number of hours later that a time in seconds
         time_2 is than a time in seconds time_1.
@@ -23,7 +24,6 @@ double hours_difference(double time_1, double time_2)
         >>> hours_difference(1800.0, 1800.0)
         0.0
     */
-    return 0.0;
 }
 
 double to_float_hours(int hours, int minutes, int seconds)
